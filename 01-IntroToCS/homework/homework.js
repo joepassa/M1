3d55646e322984045c6d78cd870ddd988a8a7ce6
joepassa <51215327+joepassa@users.noreply.github.com>
2022-08-19 22:01:26 -0300

    HW01 - Finish

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,25 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  // tu codigo acá 
+  num = num.split('').reverse();
+  let nroDecimal = 0;
+  for (let i = 0; i< num.length; i++){
+    nroDecimal += Math.pow(2,i) * parseInt(num[i]);    
+  }
+  return nroDecimal
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  // tu codigo acá
+  let binario = []
+  while (num!=0){
+    let divido = Math.floor(num/2)
+    let resto = parseInt(num%2)
+    num = divido;
+    binario.push(resto);
+  }
+  return binario.reverse().join('')
 }
 
 
